b592998ec9eeffe84f8fa93ddf88aabac9d9ba18
unknown <golovanov911@gmail.com>
2022-04-20 00:34:35 +0300

    V3

--- kopeyka.js
+++ kopeyka.js
@@ -1,6 +1,14 @@
-// отвечает за максимальное количество воодимых знаков
+// ТЗ
+
+// • Можно вводить количество копеек (число) и по клику по кнопке покажут сколько это рублей.
+// • Правила округления — обычные математические.
+// • В результате открывается поп-ап окно с результатом
+// • В результате может быть только целое число (например 1, 2, 15 рублей)
+// • Максимальное число для перевода копеек в рубли — 9999 копеек.
+
+// отвечает за максимальное количество воодимых знаков(решил обойтись без него)
 // const inputs = document.querySelectorAll('input[type=number]');
-// Array.from(inputs).forEach((input) => {
+// Array.from(inputs).forEach((input) => {ы
 //   const min = +input.min;
 //   const max = +input.max;
 
@@ -13,6 +21,7 @@
 //     }
 //   });
 // });
+
 // расчет введеного значения
 
 document.querySelector('.button').addEventListener('click', function () {
@@ -29,6 +38,13 @@ document.querySelector('.button').addEventListener('click', function () {
     document.querySelector('.input').value = null;
     document.querySelector('body').style.background = 'url(malo.jpg)';
     document.querySelector('body').style.backgroundSize = 'cover';
+  }
+  // 666
+  else if (input == 666) {
+    document.querySelector('.input').value = null;
+    document.querySelector('body').style.background = 'url(dusha.jpg)';
+    document.querySelector('body').style.backgroundSize = 'cover';
+    document.querySelector('.button').textContent = 'продать душу за копейки';
   }
   // // положительн7ый результат
   else {
